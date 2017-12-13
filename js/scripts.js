3bb7eedb1cd220305445ec22c830d58cc54f38da
Paul Kotok and Ross Fletcher <ross@>
2017-12-13 15:29:48 -0800

    add hold buttons, and js code for events happening on hold button click

--- js/scripts.js
+++ js/scripts.js
@@ -28,7 +28,32 @@ Player.prototype.rollone = function() {
   }
 }
 
+// hold
+Player.prototype.hold = function () {
+  this.totalscore += this.tempscore;
+  this.tempscore = 0;
+  // this.changeturn();
+  alert(this.playerName + ", your turn is over, pass the mouse!");
+}
 
+Player.prototype.winnerCheck = function () {
+  if (this.totalscore >= 100) {
+    alert(this.playerName + " You are the winner!");
+  }
+}
+
+Player.prototype.newGame = function () {
+  //debugger;
+  this.roll = 0;
+  this.tempscore = 0;
+  this.totalscore = 0;
+  this.playerName ="";
+}
+
+var clearValues = function(){
+  $(".player1Name").val("");
+  $(".player2Name").val("");
+}
 
 
 
@@ -53,6 +78,24 @@ $(document).ready(function() {
     player2.playerName=player2Name;
   });
 
+  $("button#new-game").click(function(event){
+    $(".playerConsole").hide();
+    clearValues();
+    player1.newGame();
+    player2.newGame();
+    $("#round-total-1").empty();
+    $("#total-score-1").empty();
+    $("#die-roll-1").empty();
+    $("#round-total-2").empty();
+    $("#total-score-2").empty();
+    $("#die-roll-2").empty();
+
+    $(".signInRow").show();
+  });
+
+
+
+
 
   $("button#player1-roll").click(function(event){
    player1.roll = rolldice();
@@ -66,5 +109,21 @@ $(document).ready(function() {
    $("#die-roll-2").text(player2.roll);
    player2.rollone();
    $("#round-total-2").text(player2.tempscore);
+  });
+
+  $("button#player1-hold").click(function(event){
+    player1.hold();
+    $("#total-score-1").text(player1.totalscore);
+    $("#round-total-1").empty();
+    $("#die-roll-1").empty();
+    player1.winnerCheck();
+  });
+
+  $("button#player2-hold").click(function(event){
+    player2.hold();
+    $("#total-score-2").text(player2.totalscore);
+    $("#round-total-2").empty();
+    $("#die-roll-2").empty();
+    player2.winnerCheck();
   });
 });
